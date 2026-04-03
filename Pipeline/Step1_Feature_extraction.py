84817bc17a48a0eb99953d55ff7d2a08b71c647f
The-System-Programmer <the.system.programmer.2004@gmail.com>
2026-04-03 19:59:12 +0530

    major change to the pipeline , no more .npz file saves , feature and description saved in dictonary

--- Pipeline/Step1_Feature_extraction.py
+++ Pipeline/Step1_Feature_extraction.py
@@ -1,26 +1,18 @@
-# Import necessary libraries
 import os
-import cv2
-import numpy as np
+import cv2 as cv
 
-def feature_extraction():
-    # List containg name of all the images
-    data_dir = "Data/Pictures"
-    files = sorted(os.listdir(data_dir))
-
-    # Make folder
-    if not os.path.exists("Data/Features"):
-        os.makedirs("Data/Features")
-
-    # Create a ORB object
-    orb = cv2.ORB_create()
-
-    # Iterate through images using list
-    features = []
+def features():
+    results = []
+    files = os.listdir("Data/Pictures")
     for f in files:
-        # Loads the image converts to gray scale
-        img = cv2.imread(f"Data/Pictures/{f}", 0)
+        path = f"Data/Pictures/{f}"
+        img = cv.imread(path, 0)
+        orb = cv.ORB_create()  # type: ignore
 
-        # Computes the keypoints and descriptions of the image
-        kp, des = orb.detectAndCompute(img, None)
-        features.append((kp,des))
+        # Keypoints
+        kp = orb.detect(img, None)
+
+        # Description
+        kp,ds = orb.compute(img, kp)
+        results.append({"name":f,"kp":kp,"ds":ds})
+    return results
